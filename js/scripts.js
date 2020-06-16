58e339ed159325c2850cf804059d128ff4230ec5
Matthew Craig <mgcraig78@gmail.com>
2020-06-16 15:56:02 -0700

    Fix branching to apply discounts
    Co-authored-by: Emma Hellman <ehh801@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -9,24 +9,24 @@ TicketBuyer.prototype.addMovie = function(ticket){
 Ticket.prototype.priceAdj = function(){
   let moviePrice = 10;
   if (this.viewerAge === "senior") {
-    moviePrice -= 1;
+    return moviePrice -= 1;
   } else if (this.viewerAge === "kid") {
-    moviePrice -= 1;
+    return moviePrice -= 1;
   } else if (this.movieTime === "morning"){
-    moviePrice -= 1;
+    return moviePrice -= 1;
   } else if (this.newRelease === true){
-    moviePrice += 1;
+    return moviePrice += 1;
   } else {
-  return this.moviePrice;
+  return "$" + moviePrice;
   }
 }
 
-function Ticket(movieName, viewerAge, movieTime, newRelease, moviePrice){
+function Ticket(movieName, viewerAge, movieTime, newRelease){
   this.movieName = movieName,
   this.viewerAge = viewerAge,
   this.movieTime = movieTime,
-  this.newRelease = newRelease,
-  this.moviePrice = moviePrice
+  this.newRelease = newRelease
+  //this.moviePrice = moviePrice
 }
 Ticket.prototype.fullReadout = function(){
   return this.movieName + " " + this.viewerAge + " " + this.movieTime + " " + this.newRelease + " " + this.moviePrice;
@@ -37,15 +37,18 @@ $(document).ready(function(){
   const inputMovieTitle = $("select#movieTitles").val();
   const inputTimeOfDay = $("select#timeOfDay").val();
   const inputAgeGroup = $("select#ageGroup").val(); 
-  let moviePrice = 10;
+  //let moviePrice = 10;
   
   event.preventDefault();
 
-  let boughtTicket = new Ticket(inputMovieTitle, inputAgeGroup, inputTimeOfDay, moviePrice);
+  let boughtTicket = new Ticket(inputMovieTitle, inputAgeGroup, inputTimeOfDay);
   $(".movie-name").text(boughtTicket.movieName)
   $(".movie-time").text(boughtTicket.movieTime)
   $(".movie-price").text(boughtTicket.priceAdj())
   $("#results").show();
-
+    console.log(inputMovieTitle);
+    console.log(inputTimeOfDay);
+    console.log(inputAgeGroup);
+    
   });
 });
