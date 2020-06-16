ab7b42ee385c032b7b0c9b1981ef6250262406f8
Matthew Craig <mgcraig78@gmail.com>
2020-06-16 11:07:02 -0700

    add initial html and js
    Co-authored-by Emma Hellman <ehh801@icloud.com>

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+//business logic
+function TicketBuyer(){
+  this.tickets = [];
+}
+
+function Ticket(movieName, viewerAge, movieTime, newRelease){
+  this.movieName = movieName,
+  this.viewerAge = viewerAge,
+  this.movieTime = movieTime,
+  this.newRelease = newRelease
+}
+
+ticket
+
+//UI logic
+$(document).ready(function(){
+  $("form#movies").submit(function(event) {
+  const selectedMovieTitle = $("select#movieTitles").val();
+  const seletedTimeOfDay = $("select#timeOfDay").val();
+  const selectedAgeGroup = $("select#ageGroup").val();  
+})
+})
+let moviePrice = 10;
+movies.forEach(function(){
+if (this.selectedAgeGroup === "senior" && selectedTimeOfDay === "evening" && newRelease === true) {
+  return (moviePrice - 1); 
+} else {
+  
+}
+})
