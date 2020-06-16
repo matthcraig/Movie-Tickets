fd79e0a2a03581f7be0c5faedd665092135d73c2
Matthew Craig <mgcraig78@gmail.com>
2020-06-16 12:59:26 -0700

    add ticket constructor and prototype
    Co-authored-by: Emma Hellman <ehh801@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,22 @@
 function TicketBuyer(){
   this.tickets = [];
 }
-
 function Ticket(movieName, viewerAge, movieTime, newRelease){
   this.movieName = movieName,
   this.viewerAge = viewerAge,
   this.movieTime = movieTime,
   this.newRelease = newRelease
-  
+
 }
 
-ticket
+Ticket.prototype.finalPrice = function(){
+  let moviePrice = 10;
+  if (this.selectedAgeGroup === "senior") {
+    moviePrice -= 1;
+  } else if {
+    ()
+  }
+}
 
 //UI logic
 $(document).ready(function(){
@@ -20,12 +26,4 @@ $(document).ready(function(){
   const seletedTimeOfDay = $("select#timeOfDay").val();
   const selectedAgeGroup = $("select#ageGroup").val();  
 })
-})
-let moviePrice = 10;
-movies.forEach(function(){
-if (this.selectedAgeGroup === "senior" && selectedTimeOfDay === "evening" && newRelease === true) {
-  return (moviePrice - 1); 
-} else {
-  
-}
 })
